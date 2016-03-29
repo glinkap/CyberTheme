31625ad96dba3a4524c52f7a2e5ba95ef12d2bc6
Ageev Igor <i.ageev@cyberplat.ru>
2016-03-29 14:44:30 +0300

    Adds two plugins to build system: autoprefixer and detecter of changed files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@
 var gulp = require('gulp'),
 		sass = require('gulp-sass'),
 		csso = require('gulp-csso'),
+		autoprefixer = require('gulp-autoprefixer'),
 		fontmin = require('gulp-fontmin'),
 		del = require('del'),
 		svg2png = require('gulp-svg2png'),
 		svgmin = require('gulp-svgmin'),
+		changed = require('gulp-changed'),
 		connect = require('gulp-connect');
 
 /*
@@ -29,6 +31,7 @@ gulp.task('html', function () {
 gulp.task('style', function() {
 	gulp.src('./source/styles/*.scss')
 	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer())
 	.pipe(csso())
 	.pipe(gulp.dest('./assets/css/'))
 	.pipe(connect.reload());
@@ -51,15 +54,17 @@ gulp.task('font:build', function () {
 /*
 * Подготавливаем изображения
 */
-gulp.task('image', ['image:copy'], function () {
+gulp.task('image', ['image:rasterize'], function () {
 	gulp.src('./source/images/**/*.svg')
-	.pipe(svg2png())
+	.pipe(changed('./assets/img/'))
+	.pipe(svgmin())
 	.pipe(gulp.dest('./assets/img/'));
 });
 
-gulp.task('image:copy', function () {
+gulp.task('image:rasterize', function () {
 	return gulp.src('./source/images/**/*.svg')
-	.pipe(svgmin())
+	.pipe(changed('./assets/img/'))
+	.pipe(svg2png())
 	.pipe(gulp.dest('./assets/img/'));
 });
 
