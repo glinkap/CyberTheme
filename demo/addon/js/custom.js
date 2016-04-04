b264d6b1d31a353954839ec68a9f7100e222bbe6
Ageev Igor <i.ageev@cyberplat.ru>
2016-04-04 10:30:47 +0300

    Changes style of code (tabs to spaces).

--- demo/addon/js/custom.js
+++ demo/addon/js/custom.js
@@ -1,56 +1,56 @@
-(function(){
+(function() {
 
-	$("a[href='#']").click(function(e) {
-		e.preventDefault();
-	});
+  $("a[href='#']").click(function(e) {
+    e.preventDefault();
+  });
 
-	var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
-		var html = $(this).parent().html();
-		html = cleanSource(html);
-		$("#source-modal pre code").text(html);
-		// format code and hightlighter
-		Prism.highlightElement($("#source-modal pre code")[0]);
-		$("#source-modal").modal();
-	});
-	$('.bs-component [data-toggle="popover"]').popover();
-	$('.bs-component [data-toggle="tooltip"]').tooltip();
-	$(".bs-component").hover(function(){
-		$(this).append($button);
-		$button.show();
-	}, function(){
-		$button.hide();
-	});
+  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function() {
+    var html = $(this).parent().html();
+    html = cleanSource(html);
+    $("#source-modal pre code").text(html);
+    // format code and hightlighter
+    Prism.highlightElement($("#source-modal pre code")[0]);
+    $("#source-modal").modal();
+  });
+  $('.bs-component [data-toggle="popover"]').popover();
+  $('.bs-component [data-toggle="tooltip"]').tooltip();
+  $(".bs-component").hover(function() {
+    $(this).append($button);
+    $button.show();
+  }, function() {
+    $button.hide();
+  });
 
-	function cleanSource(html) {
-		html = html.replace(/×/g, "&close;")
-		.replace(/«/g, "&laquo;")
-		.replace(/»/g, "&raquo;")
-		.replace(/←/g, "&larr;")
-		.replace(/→/g, "&rarr;");
-		var lines = html.split(/\n/);
-		lines.shift();
-		lines.splice(-1, 1);
-		var tabsChar = lines[0].substring(0,1),
-		reCount = new RegExp("[^" + tabsChar + "]"),
-		indentSize = lines[0].split(reCount)[0].length,
-		re = new RegExp(tabsChar + "{" + indentSize + "}");
-		lines = lines.map(function(line){
-			if (line.match(re)) {
-				line = line.substring(indentSize);
-			}
-			return line;
-		});
-		lines = lines.join("\n");
-		return lines;
-	}
+  function cleanSource(html) {
+    html = html.replace(/×/g, "&close;")
+      .replace(/«/g, "&laquo;")
+      .replace(/»/g, "&raquo;")
+      .replace(/←/g, "&larr;")
+      .replace(/→/g, "&rarr;");
+    var lines = html.split(/\n/);
+    lines.shift();
+    lines.splice(-1, 1);
+    var tabsChar = lines[0].substring(0, 1),
+      reCount = new RegExp("[^" + tabsChar + "]"),
+      indentSize = lines[0].split(reCount)[0].length,
+      re = new RegExp(tabsChar + "{" + indentSize + "}");
+    lines = lines.map(function(line) {
+      if (line.match(re)) {
+        line = line.substring(indentSize);
+      }
+      return line;
+    });
+    lines = lines.join("\n");
+    return lines;
+  }
 
-	var clipboard = new Clipboard('#icons a', {
-		text: function(trigger) {
-			//var result = trigger.innerHTML.replace(/\s{2,}/g, '');
-			var result = trigger.innerHTML.match(/<span class="\S*"><\/span>/);
-			return result;
-		}
-	});
+  var clipboard = new Clipboard('#icons a', {
+    text: function(trigger) {
+      //var result = trigger.innerHTML.replace(/\s{2,}/g, '');
+      var result = trigger.innerHTML.match(/<span class="\S*"><\/span>/);
+      return result;
+    }
+  });
 
 
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,103 @@
-'use strict';
-
 var gulp = require('gulp'),
-		sass = require('gulp-sass'),
-		csso = require('gulp-csso'),
-		autoprefixer = require('gulp-autoprefixer'),
-		fontmin = require('gulp-fontmin'),
-		del = require('del'),
-		svg2png = require('gulp-svg2png'),
-		svgmin = require('gulp-svgmin'),
-		changed = require('gulp-changed'),
-		connect = require('gulp-connect');
+  sass = require('gulp-sass'),
+  csso = require('gulp-csso'),
+  autoprefixer = require('gulp-autoprefixer'),
+  fontmin = require('gulp-fontmin'),
+  del = require('del'),
+  svg2png = require('gulp-svg2png'),
+  svgmin = require('gulp-svgmin'),
+  changed = require('gulp-changed'),
+  connect = require('gulp-connect');
 
 /*
-* Запуск вебсервера
-*/
+ * Запуск вебсервера
+ */
 gulp.task('webserver', function() {
-	connect.server({
-		livereload: true
-	});
+  connect.server({
+    livereload: true
+  });
 });
 
-gulp.task('html', function () {
-	gulp.src('./demo/*.html')
-	.pipe(connect.reload());
+gulp.task('html', function() {
+  gulp.src('./demo/*.html')
+    .pipe(connect.reload());
 });
 
 /*
-* Генерируем стилевые файлы
-*/
+ * Генерируем стилевые файлы
+ */
 gulp.task('style', function() {
-	gulp.src('./source/styles/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(autoprefixer())
-	.pipe(csso())
-	.pipe(gulp.dest('./assets/css/'))
-	.pipe(connect.reload());
+  gulp.src('./source/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(gulp.dest('./assets/css/'))
+    .pipe(connect.reload());
 });
 
 /*
-* Генерируем веб-шрифты
-*/
-gulp.task('font', ['font:build'], function () {
-	// Нужно прибраться после генератора шрифтов
-	del(['./assets/fonts/*.css']);
+ * Генерируем веб-шрифты
+ */
+gulp.task('font', ['font:build'], function() {
+  // Нужно прибраться после генератора шрифтов
+  del(['./assets/fonts/*.css']);
 });
 
-gulp.task('font:build', function () {
-	return gulp.src('./source/fonts/*.ttf')
-	.pipe(fontmin())
-	.pipe(gulp.dest('./assets/fonts/'));
-});
-
-/*
-* Подготавливаем изображения
-*/
-gulp.task('image', ['image:rasterize'], function () {
-	gulp.src('./source/images/**/*.svg')
-	.pipe(changed('./assets/img/'))
-	.pipe(svgmin())
-	.pipe(gulp.dest('./assets/img/'));
-});
-
-gulp.task('image:rasterize', function () {
-	return gulp.src('./source/images/**/*.svg')
-	.pipe(changed('./assets/img/'))
-	.pipe(svg2png())
-	.pipe(gulp.dest('./assets/img/'));
+gulp.task('font:build', function() {
+  return gulp.src('./source/fonts/*.ttf')
+    .pipe(fontmin())
+    .pipe(gulp.dest('./assets/fonts/'));
 });
 
 /*
-* Копируем зависимости из bower_components
-*/
+ * Подготавливаем изображения
+ */
+gulp.task('image', ['image:rasterize'], function() {
+  gulp.src('./source/images/**/*.svg')
+    .pipe(changed('./assets/img/'))
+    .pipe(svgmin())
+    .pipe(gulp.dest('./assets/img/'));
+});
+
+gulp.task('image:rasterize', function() {
+  return gulp.src('./source/images/**/*.svg')
+    .pipe(changed('./assets/img/'))
+    .pipe(svg2png())
+    .pipe(gulp.dest('./assets/img/'));
+});
+
+/*
+ * Копируем зависимости из bower_components
+ */
 gulp.task('vendor', ['vendor:css'], function() {
-	gulp.src([
-		'./bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-		'./bower_components/jquery/dist/jquery.min.js',
-		'./bower_components/prism/prism.js',
-		'./bower_components/clipboard/dist/clipboard.min.js',
-		'./bower_components/html5shiv/dist/html5shiv.min.js',
-		'./bower_components/respond/dest/respond.min.js'
-	])
-	.pipe(gulp.dest('./vendor/js'));
+  gulp.src([
+      './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+      './bower_components/jquery/dist/jquery.min.js',
+      './bower_components/prism/prism.js',
+      './bower_components/clipboard/dist/clipboard.min.js',
+      './bower_components/html5shiv/dist/html5shiv.min.js',
+      './bower_components/respond/dest/respond.min.js'
+    ])
+    .pipe(gulp.dest('./vendor/js'));
 });
 
 gulp.task('vendor:css', function() {
-	return gulp.src([
-		'./bower_components/prism/themes/prism.css'
-	])
-	.pipe(gulp.dest('./vendor/css'));
+  return gulp.src([
+      './bower_components/prism/themes/prism.css'
+    ])
+    .pipe(gulp.dest('./vendor/css'));
 });
 
 /*
-* Запуск слежки за исходниками
-*/
+ * Запуск слежки за исходниками
+ */
 gulp.task('watch', function() {
   gulp.watch('./source/styles/**/*.scss', ['style']);
-	gulp.watch(['./demo/*.html'], ['html']);
-	gulp.watch(['./source/fonts/*.ttf'], ['font']);
+  gulp.watch(['./demo/*.html'], ['html']);
+  gulp.watch(['./source/fonts/*.ttf'], ['font']);
 });
 
 /*
-* Начало работы
-*/
+ * Начало работы
+ */
 gulp.task('default', ['webserver', 'watch']);
